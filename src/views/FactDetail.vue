3015da59e0ce8d866e51df9d81224088b43dc37a
Andreas <a.christiansen1995@gmail.com>
2024-02-20 17:14:32 +0100

    Update details page

--- src/views/FactDetail.vue
+++ src/views/FactDetail.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import FavouriteFact from "@/components/FavouriteFact.vue";
+import ImageBlock from "@/components/ImageBlock.vue";
 import {
   IonPage,
   IonHeader,
@@ -8,9 +9,9 @@ import {
   IonContent,
   IonGrid,
   IonRow,
-  IonImg,
   IonButtons,
   IonBackButton,
+  IonCol,
 } from "@ionic/vue";
 import { useFactsStore } from "@/stores/useFactsStore";
 
@@ -28,22 +29,23 @@ const { currentFact } = useFactsStore();
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-grid>
-        <ion-row>
-          <ion-col>
-            <div class="details">
+      <div class="details">
+        <ion-grid>
+          <ion-row>
+            <ion-col class="ion-text-end">
               <favourite-fact :fact="currentFact" />
-              <ion-img
-                class="details__image"
-                src="https://cataas.com/cat"
-                alt="Random Cat From Cataas"
-              ></ion-img>
-
-              {{ currentFact }}
-            </div>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <image-block />
+              <p>
+                {{ currentFact }}
+              </p>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -52,11 +54,5 @@ const { currentFact } = useFactsStore();
 .details {
   display: grid;
   gap: 20px;
-
-  &__image {
-    width: 100%;
-    height: 300px;
-    object-fit: cover;
-  }
 }
 </style>
